cd0ac1e3d0b0c3c37ddc93d97d0146c2b05d53b7
Quality Gate Proxy <qg.proxy@frontendart.com>
2023-02-22 21:29:36 +0000

    Repair with creating new Date

--- src/main/java/org/apache/dolphinscheduler/dao/entity/TaskInstance.java
+++ src/main/java/org/apache/dolphinscheduler/dao/entity/TaskInstance.java
@@ -372,7 +372,7 @@ public class TaskInstance implements Serializable {
     }
 
     public Date getEndTime() {
-        return endTime;
+        return new Date(endTime.getTime());
     }
 
     public void setEndTime(Date endTime) {

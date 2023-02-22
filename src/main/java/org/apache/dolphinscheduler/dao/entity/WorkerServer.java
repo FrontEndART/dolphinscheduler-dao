5123a835efdca123024a8f9baa32a1a3ff8a99b7
Quality Gate Proxy <qg.proxy@frontendart.com>
2023-02-22 21:23:35 +0000

    Repair with clone

--- src/main/java/org/apache/dolphinscheduler/dao/entity/WorkerServer.java
+++ src/main/java/org/apache/dolphinscheduler/dao/entity/WorkerServer.java
@@ -89,7 +89,7 @@ public class WorkerServer {
     }
 
     public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
+        this.createTime = (Date)createTime.clone();
     }
 
     public String getZkDirectory() {

e6527278ad46ddb0235f16a2d2fc5451def288a2
Quality Gate Proxy <qg.proxy@frontendart.com>
2023-02-22 21:16:56 +0000

    Repair with clone

--- src/main/java/org/apache/dolphinscheduler/dao/entity/DataSource.java
+++ src/main/java/org/apache/dolphinscheduler/dao/entity/DataSource.java
@@ -142,7 +142,7 @@ public class DataSource {
   }
 
   public void setCreateTime(Date createTime) {
-    this.createTime = createTime;
+    this.createTime = (Date)createTime.clone();
   }
 
   public Date getUpdateTime() {

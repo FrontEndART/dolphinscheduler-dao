e9662ac96a8219684eaeee0c51467f516dc692ae
Quality Gate Proxy <qg.proxy@frontendart.com>
2023-02-22 21:21:05 +0000

    Repair with creating new Date

--- src/main/java/org/apache/dolphinscheduler/dao/entity/ResourcesUser.java
+++ src/main/java/org/apache/dolphinscheduler/dao/entity/ResourcesUser.java
@@ -107,7 +107,7 @@ public class ResourcesUser {
   }
 
   public void setUpdateTime(Date updateTime) {
-    this.updateTime = updateTime;
+    this.updateTime = new Date(updateTime.getTime());
   }
 
   @Override

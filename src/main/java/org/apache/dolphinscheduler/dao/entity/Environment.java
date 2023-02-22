472c0bf2c85eae047c499cb72ceea7033774ef15
Quality Gate Proxy <qg.proxy@frontendart.com>
2023-02-22 21:25:48 +0000

    Repair with clone

--- src/main/java/org/apache/dolphinscheduler/dao/entity/Environment.java
+++ src/main/java/org/apache/dolphinscheduler/dao/entity/Environment.java
@@ -118,7 +118,7 @@ public class Environment {
     }
 
     public Date getUpdateTime() {
-        return updateTime;
+        return (Date)updateTime.clone();
     }
 
     public void setUpdateTime(Date updateTime) {

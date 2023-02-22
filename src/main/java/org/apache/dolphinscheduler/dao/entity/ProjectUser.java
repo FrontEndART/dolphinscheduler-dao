173f8800220ca06883eebaa1163849fd2a5c48fa
Quality Gate Proxy <qg.proxy@frontendart.com>
2023-02-22 21:27:56 +0000

    Repair with clone

--- src/main/java/org/apache/dolphinscheduler/dao/entity/ProjectUser.java
+++ src/main/java/org/apache/dolphinscheduler/dao/entity/ProjectUser.java
@@ -85,7 +85,7 @@ public class ProjectUser {
     }
 
     public Date getCreateTime() {
-        return createTime;
+        return (Date)createTime.clone();
     }
 
     public void setCreateTime(Date createTime) {

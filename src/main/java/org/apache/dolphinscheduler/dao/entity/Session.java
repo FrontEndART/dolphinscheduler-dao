92b3b9277d3e5c62b7348016fd79c5dc54c7390a
Quality Gate Proxy <qg.proxy@frontendart.com>
2023-02-22 21:21:32 +0000

    Repair with creating new Date

--- src/main/java/org/apache/dolphinscheduler/dao/entity/Session.java
+++ src/main/java/org/apache/dolphinscheduler/dao/entity/Session.java
@@ -80,7 +80,7 @@ public class Session {
     }
 
     public void setLastLoginTime(Date lastLoginTime) {
-        this.lastLoginTime = lastLoginTime;
+        this.lastLoginTime = new Date(lastLoginTime.getTime());
     }
 
     @Override

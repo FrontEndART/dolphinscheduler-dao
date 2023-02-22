d3be626cf03af0b93507b2e2158b856aefe522d5
Quality Gate Proxy <qg.proxy@frontendart.com>
2023-02-22 21:15:47 +0000

    Repair with creating new Date

--- src/main/java/org/apache/dolphinscheduler/dao/entity/Alert.java
+++ src/main/java/org/apache/dolphinscheduler/dao/entity/Alert.java
@@ -140,7 +140,7 @@ public class Alert {
     }
 
     public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
+        this.updateTime = new Date(updateTime.getTime());
     }
 
     public Map<String, Object> getInfo() {

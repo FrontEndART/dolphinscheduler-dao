e045f82a225e1b50ca1f59183da3921fda94dee3
Quality Gate Proxy <qg.proxy@frontendart.com>
2023-02-22 21:29:27 +0000

    Repair with clone

--- src/main/java/org/apache/dolphinscheduler/dao/entity/TaskDefinitionLog.java
+++ src/main/java/org/apache/dolphinscheduler/dao/entity/TaskDefinitionLog.java
@@ -83,7 +83,7 @@ public class TaskDefinitionLog extends TaskDefinition {
     }
 
     public Date getOperateTime() {
-        return operateTime;
+        return (Date)operateTime.clone();
     }
 
     public void setOperateTime(Date operateTime) {

bd57f06da459d25964f96fbc0f7af0bef0aa5bf6
Quality Gate Proxy <qg.proxy@frontendart.com>
2023-02-22 21:26:11 +0000

    Repair with clone

--- src/main/java/org/apache/dolphinscheduler/dao/entity/ErrorCommand.java
+++ src/main/java/org/apache/dolphinscheduler/dao/entity/ErrorCommand.java
@@ -219,7 +219,7 @@ public class ErrorCommand {
     }
 
     public Date getStartTime() {
-        return startTime;
+        return (Date)startTime.clone();
     }
 
     public void setStartTime(Date startTime) {

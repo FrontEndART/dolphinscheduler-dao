7a045bc5b6b569adf2128623f884c0affeaa67ab
Quality Gate Proxy <qg.proxy@frontendart.com>
2023-02-22 21:30:34 +0000

    Repair with clone

--- src/main/java/org/apache/dolphinscheduler/dao/entity/UdfFunc.java
+++ src/main/java/org/apache/dolphinscheduler/dao/entity/UdfFunc.java
@@ -180,7 +180,7 @@ public class UdfFunc {
     }
 
     public Date getCreateTime() {
-        return createTime;
+        return (Date)createTime.clone();
     }
 
     public void setCreateTime(Date createTime) {

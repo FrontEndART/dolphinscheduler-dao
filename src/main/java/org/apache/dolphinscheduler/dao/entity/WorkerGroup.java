ab3ebfbcf932ebdbed886b7d94b4b1d324430776
Quality Gate Proxy <qg.proxy@frontendart.com>
2023-02-22 21:23:24 +0000

    Repair with clone

--- src/main/java/org/apache/dolphinscheduler/dao/entity/WorkerGroup.java
+++ src/main/java/org/apache/dolphinscheduler/dao/entity/WorkerGroup.java
@@ -76,7 +76,7 @@ public class WorkerGroup {
     }
 
     public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
+        this.createTime = (Date)createTime.clone();
     }
 
     public Date getUpdateTime() {
